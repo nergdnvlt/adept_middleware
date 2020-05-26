2c64395108f4fa9a8d8e2c5d68f56c3d5db201b5
Tyler Lundgren <tylerlundgren@yahoo.com>
2020-05-26 13:03:54 -0600

    Trying to fix for heroku.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,4 @@
 class PagesController < ApplicationController
-  def welcome; end
+  def welcome
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#welcome'
-  
+
   namespace :api do
     namespace :v1 do
       post '/accounts', to: 'fastspring#accounts'
